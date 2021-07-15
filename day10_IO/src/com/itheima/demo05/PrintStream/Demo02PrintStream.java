fdd4de2b2f3a6396ce480b8d200180ec38d94e02
PengHengBen <1735416175@qq.com>
2021-07-16 00:11:42 +0800

    20210715 提交

---
+++ day10_IO/src/com/itheima/demo05/PrintStream/Demo02PrintStream.java
@@ -0,0 +1,24 @@
+package com.itheima.demo05.PrintStream;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+/**
+ * @author heng.peng
+ * @date 2021/7/15 - 21:54
+ * 可以改变输出语句的目的地（打印流的流向）
+ * 输出语句，默认在控制台输出
+ * 使用System.setOut方法改变输出语句的目的地改为参数中传递的打印流的目的地
+ *      static void setOut(PrintStream out) 重新分配“标准”输出流。
+ */
+public class Demo02PrintStream {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("我是在控制台输出的");
+
+        PrintStream ps = new PrintStream("day10_IO\\stream.txt");
+        System.setOut(ps); // 把输出语句的目的地改为参数中传递的打印流的目的地
+        System.out.println("我在打印流的目的地中输出");
+
+        ps.close();
+    }
+}
